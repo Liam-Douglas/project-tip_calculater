071d95779738ba9b7e9e421d5eac7df5315fbd43
Liam Douglas <liam.douglas@outlook.com.au>
2023-06-08 08:48:04 +1000

    My first commit

--- main.py
+++ main.py
@@ -4,11 +4,19 @@
 #Format the result to 2 decimal places = 33.60
 
 #write code below
+bill = float(input("What was the total Bill? "))
+people = int(input("How many people were there? "))
+tip = int(input("what is the tip perctange? "))
 
-x = float(input("What was the Bill? "))
-y = int(input("fHow many people were there? "))
-z = float(input("what is the tip perctange? "))
+# Calculate tip as percentage
+tip_as_percent = tip / 100
+# Calculate total tip amount
+total_tip_amount = bill * tip_as_percent
+# Calculate the total bill from the bill and tip amount
+total_bill = bill + total_tip_amount
+# Calculate bill amount per person
+bill_per_person = total_bill / people
+# the Final amount then rounded by 2 digits
+final_amount = round(bill_per_person, 2)
 
-tip = (x / y) * z
-
-print (float(tip))
+print (f"Everyone should pay ${final_amount}")
